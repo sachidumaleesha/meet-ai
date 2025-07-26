33b688253c34d1b3cc8926e5e60179dd4615e974
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2025-07-26 10:41:33 +0530

    22: fix UI issues on call-active screen

--- src/modules/call/ui/components/call-active.tsx
+++ src/modules/call/ui/components/call-active.tsx
@@ -11,17 +11,17 @@ interface Props {
 export const CallActive = ({ onLeave, meetingName }: Props) => {
   return (
     <div className="flex flex-col justify-between p-4 h-full text-white">
-      <div className="bg-[#101213] rounded-full p-4 flex items-center gap-4">
+      <div className="bg-[#101213] rounded-full p-4 flex items-center gap-4 w-full">
         <Link
           href="/"
-          className="flex items-center justify-center p-1 bg-white/10 rounded-full w-fit"
+          className="flex items-center justify-center gap-1 px-3 py-1 bg-white/10 rounded-full w-fit"
         >
           <Image src="/logo.svg" width={22} height={22} alt="Logo" />
           <h4 className="text-base">{meetingName}</h4>
         </Link>
       </div>
       <SpeakerLayout />
-      <div className="bg-[#101213] rounded-full p-4">
+      <div className="bg-[#101213] rounded-full px-4 w-full">
         <CallControls onLeave={onLeave} />
       </div>
     </div>
